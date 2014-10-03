52c1cef0bfd2e67f477186e135e9edaf7f2e7b71
Dan Vanderkam <danvdk@gmail.com>
2014-10-02 20:48:07 -0400

    New idea about how to do this: marks

--- cli.py
+++ cli.py
@@ -9,67 +9,9 @@ import gflags
 FLAGS = gflags.FLAGS
 
 
-def nodes_to_paths(nodes):
-    '''Convert all nodes to paths.'''
-    # TODO: de-dupe
-    return [node_to_path(node) for node in nodes]
-
-
-def node_to_path(node):
-    '''Convert a jsonselect.Parser node to a path from root to that node.
-
-    This returns an array of indices and strings, where there are either
-    indexes into an array or keys in an object depending on whether they're
-    strings or integers.
-
-    Indices are 1-based!
-    '''
-    component = node.idx or node.parent_key
-    rest = node_to_path(node.parent) if node.parent else []
-    return rest + ([component] if component else [])
-
-
-def apply_selector(selector, obj):
+def selector_to_ids(selector, obj):
     nodes = get_result_nodes(selector, obj)
-    paths = nodes_to_paths(nodes)
-
-
-def select_path(path, in_obj, out_obj):
-    '''Copy the portion of in_obj along path into out_obj.
-
-    Note: we fill out an object rather than returning one to facilitate
-    accumulating an output object from many paths.
-    '''
-    if len(path) == 0:
-        raise ValueError("Can't select empty path.")
-
-    component = path[0]
-    if type(component) == int: component -= 1  # stupid 1-based CSS!
-    if len(path) == 1:
-        if type(component) == int:
-            out_obj.append(in_obj[component])
-        else:
-            out_obj[component] = in_obj[component]
-    else:
-        selected = in_obj[component]
-        try:
-            new_out = out_obj[component]
-            select_path(path[1:], selected, new_out)
-        except (IndexError, KeyError):
-            new_out = [] if isinstance(selected, list) else {}
-            select_path(path[1:], selected, new_out)
-            if type(component) == int:
-                out_obj.append(new_out)
-            else:
-                out_obj[component] = new_out
-
-
-def select_paths(paths, obj):
-    '''Returns the union of all the paths selected on obj.'''
-    base = [] if isinstance(obj, list) else {}
-    for path in paths:
-        select_path(path, obj, base)
-    return base
+    return [id(node.parent.value) for node in nodes]
 
 
 def get_result_nodes(selector, obj):
@@ -123,11 +65,32 @@ def filter_object(obj, marks, presumption=DELETE):
                 del obj[k]
 
 
+def usage():
+    print '''%s [selector] [-v exclude_selector]
+
+...
+'''
+
+
 if __name__ == '__main__':
-    assert len(sys.argv) == 3
-    _, selector, json_file = sys.argv
+    path = sys.argv.pop()  # TODO: allow stdin
+
+    actions = sys.argv[1:]
 
     obj = json.load(open(json_file))
+    while actions:
+        action = actions[0]
+        del actions[0]
+        mode = KEEP
+        if action == '-v':
+            mode = DELETE
+            action = actions[0]
+            del actions[0]
+
+        marks = {k: action for k in selector_to_ids(action, obj)}
+        filter_object(obj, marks, presumption=(2 - mode))
+
+
     result = apply_selector(selector, obj)
 
     print json.dumps(result, indent=2)
--- tests/cli_test.py
+++ tests/cli_test.py
@@ -4,74 +4,6 @@ import cli
 import json
 
 
-def test_node_to_path():
-    obj = json.load(open('tests/data.json'))
-    nodes = cli.get_result_nodes('.owner', obj)
-    eq_(1, len(nodes))
-
-    eq_(['wut', 'metadata', 'owner'], cli.node_to_path(nodes[0]))
-
-    nodes = cli.get_result_nodes('.foo', obj)
-    eq_(1, len(nodes))
-    eq_(['foo'], cli.node_to_path(nodes[0]))
-
-    nodes = cli.get_result_nodes('.baz', obj)
-    eq_(1, len(nodes))
-    eq_(['foo', 2, 'baz'], cli.node_to_path(nodes[0]))
-
-
-def test_select_path_dict():
-    obj = { 'foo': { 'bar': 'baz' }, 'quux': 'wut' }
-    out = {}
-    cli.select_path(['foo', 'bar'], obj, out)
-    eq_({'foo': {'bar': 'baz'}}, out)
-
-    out = {}
-    cli.select_path(['quux'], obj, out)
-    eq_({'quux': 'wut'}, out)
-
-
-def test_select_path_int():
-    obj = [ 'a', 'b', 'c' ]
-    out = []
-    cli.select_path([1], obj, out)
-    eq_(['a'], out)
-    cli.select_path([3], obj, out)
-    eq_(['a', 'c'], out)
-
-
-def test_mixed_select():
-    obj = { 'foo': [ { 'bar': 'baz', 'a': 'b' }, { 'x': 'y', 'z': 4 } ] }
-    out = {}
-    cli.select_path(['foo', 2, 'z'], obj, out)
-    eq_({'foo': [{ 'z': 4 }]}, out)
-
-    obj = {'foo': ['bar', {'baz': 'quux'}]}
-    out = {}
-    cli.select_path(['foo', 1], obj, out)
-    eq_({'foo': ['bar']}, out)
-
-
-def test_nonempty_out():
-    '''cli.select_path should preserve any objects already in out_obj.'''
-    obj = ['bar', {'baz': 'quux'}]
-    out = ['bar']
-    cli.select_path([2], obj, out)
-    eq_(['bar', {'baz': 'quux'}], out)
-
-    obj = {'foo': ['bar', {'baz': 'quux'}]}
-    out = {'foo': ['bar']}
-
-    cli.select_path(['foo', 2], obj, out)
-    eq_({'foo': ['bar', {'baz': 'quux'}]}, out)
-
-
-def test_select_paths():
-    obj = json.load(open('tests/data.json'))
-    eq_({'foo': ['bar', {'baz': 'quux'}]},
-        cli.select_paths([['foo', 1], ['foo', 2, 'baz']], obj))
-
-
 def test_filter_object():
     obj = {'foo': ['bar', {'baz': 'quux'}]}
     cli.filter_object(obj, {id(obj['foo'][0]): cli.KEEP})
@@ -84,3 +16,8 @@ def test_filter_object():
     obj = {'foo': ['bar', {'baz': 'quux'}]}
     cli.filter_object(obj, {id(obj['foo']): cli.DELETE})
     eq_({}, obj)
+
+    # knock out a particular field.
+    obj = {'foo': ['bar', {'baz': 'quux'}]}
+    cli.filter_object(obj, {id(obj['foo'][1]): cli.DELETE}, presumption=cli.KEEP)
+    eq_({'foo': ['bar']}, obj)
