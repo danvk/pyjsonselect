581353c5fab2713bf1095166a44e84e5dff2dd04
Dan Vanderkam <danvdk@gmail.com>
2014-10-04 00:39:34 -0400

    begin copying parser tests

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,7 +1,16 @@
 from nose.tools import *
 
-from jsonselectjs import parse
+from jsonselectjs import parse, JsonSelectError
 
-def test_two_ids():
-    eq_([9,[{"id":"foo"},{"id":"bar"}]],
-        parse('.foo .bar'))
+def test_Selectors():
+    eq_([4, [{'id': "foo"}]], parse(".foo"))
+    eq_([8, [{'id': " foo "}]], parse('." foo "'))
+    eq_([21, [{'a': 0, 'b': 1, 'id': "foo", 'pf': ":nth-last-child", 'type': "string"}]], parse("string.foo:last-child"))
+    eq_([15, [{'id': "xxx@yyy", 'type': "string"}]], parse("string.xxx\\@yyy"))
+    with assert_raises(JsonSelectError) as context:
+        parse(" ")
+    eq_("selector expected in ' '", context.exception.message)
+
+    with assert_raises(JsonSelectError) as context:
+        parse("")
+    eq_("selector expected", context.exception.message)
