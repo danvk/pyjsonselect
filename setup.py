0d550d9de4e0ce4b311ee66c45b5a516da5faa2c
Dan Vanderkam <danvdk@gmail.com>
2014-10-06 18:48:26 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='pyjsonselect',
+      version='0.1.0',
+      description='Fully-compliant implementation of JSONSelect',
+      author='Dan Vanderkam',
+      author_email='danvdk@gmail.com',
+      url='https://github.com/danvk/pyjsonselect/',
+      packages=find_packages(exclude=['tests*']),
+      install_requires=[],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Topic :: Utilities',
+          'Topic :: Text Processing'
+      ],
+      keywords=[
+          'css',
+          'json',
+          'jsonselect'
+      ]
+)
