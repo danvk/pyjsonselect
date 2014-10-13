eac9f3642c7b5f49b359a5caf48f21f373b11e3d
Dan Vanderkam <danvdk@gmail.com>
2014-10-13 17:38:55 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyjsonselect',
-      version='0.1.0',
+      version='0.2.0',
       description='Fully-compliant implementation of JSONSelect',
       author='Dan Vanderkam',
       author_email='danvdk@gmail.com',
